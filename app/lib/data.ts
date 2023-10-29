85e62954bec2056910f71ac6469f0363d1779be6
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2023-10-29 20:03:34 +0900

    chapter-9: streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,15 +18,16 @@ export async function fetchRevenue() {
   noStore()
 
   try {
+    // ここはStreamingというできたものから順次送ってくれる仕組みで解消するとchapter-9で説明あり
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch complete after 3 seconds.');
+    console.log('Data fetch complete after 3 seconds.');
 
     return data.rows;
   } catch (error) {
