c0fc74e480e56e42935c29dc0e8edf96528e8f2b
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2023-10-29 21:21:54 +0900

    chapter-11: adding-search-and-pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) {
   return <p>Customers Page</p>;
 }
